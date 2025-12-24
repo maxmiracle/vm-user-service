a19ceae9b625fcf4de8dbea6d21cfcc5344c017e
maxmiracle <vasiliev.maxim@gmail.com>
2025-12-25 01:36:27 +0300

    Initial commit, Spring initializer without editing.

---
+++ src/main/java/ru/maximserver/vmuserservice/VmUserServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.maximserver.vmuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmUserServiceApplication.class, args);
+	}
+
+}
